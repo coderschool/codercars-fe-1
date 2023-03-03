5a7cd6e33aeb4cd92080d839e75b0c560a637054
Anh Ky Nguyen <anh163@gmail.com>
2023-03-03 22:50:20 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './routes';
-import ThemeProvider from './theme';
-import './App.css';
-import { Box, CssBaseline, Typography } from '@mui/material';
-import Copyright from './components/Copyright';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import ThemeProvider from "./theme";
+import "./App.css";
+import { Box, CssBaseline, Typography } from "@mui/material";
+import Copyright from "./components/Copyright";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<ThemeProvider>
-				<CssBaseline />
-				<Box className="hero-image" mb={2} display="flex" justifyContent="center" alignItems="center">
-					<Typography variant="h1" color="white" className="title">
-						CODERCAR DATABASE
-					</Typography>
-				</Box>
-				<Router />
-				<Copyright />
-			</ThemeProvider>
-		</BrowserRouter>
-	);
+  console.log(process.env.REACT_APP_BACKEND_API);
+  return (
+    <BrowserRouter>
+      <ThemeProvider>
+        <CssBaseline />
+        <Box
+          className="hero-image"
+          mb={2}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h1" color="white" className="title">
+            CODERCAR DATABASE
+          </Typography>
+        </Box>
+        <Router />
+        <Copyright />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
